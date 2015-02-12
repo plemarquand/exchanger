181acbe60dc3b390e54039b149be1d0516905347
Paul LeMarquand <plemarquand@qnx.com>
2015-02-12 12:39:38 -0500

    Using NTLM auth, adding getCalendarItems

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ exports.client = null;
 
 
 exports.initialize = function(settings, callback) {
-  var soap = require('soap');
+  var soap = require('soap-ntlm');
   // TODO: Handle different locations of where the asmx lives.
   var endpoint = 'https://' + path.join(settings.url, 'EWS/Exchange.asmx');
   var url = path.join(__dirname, 'Services.wsdl');
@@ -23,7 +23,7 @@ exports.initialize = function(settings, callback) {
     }
 
     exports.client = client;
-    exports.client.setSecurity(new soap.BasicAuthSecurity(settings.username, settings.password));
+    exports.client.setSecurity(new soap.NtlmSecurity(settings.username, settings.password));
 
     return callback(null);
   }, endpoint);
@@ -44,7 +44,7 @@ exports.getEmails = function(folderName, limit, callback) {
     return callback(new Error('Call initialize()'));
   }
 
-  var soapRequest = 
+  var soapRequest =
     '<tns:FindItem Traversal="Shallow" xmlns:tns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
       '<tns:ItemShape>' +
         '<t:BaseShape>IdOnly</t:BaseShape>' +
@@ -64,12 +64,12 @@ exports.getEmails = function(folderName, limit, callback) {
           '<t:FieldURI FieldURI="item:Importance"></t:FieldURI>' +
           '<t:FieldURI FieldURI="item:Subject"></t:FieldURI>' +
           '<t:FieldURI FieldURI="item:DateTimeReceived"></t:FieldURI>' +
-        '</t:AdditionalProperties>' + 
+        '</t:AdditionalProperties>' +
       '</tns:ItemShape>' +
       '<tns:IndexedPageItemView BasePoint="Beginning" Offset="0" MaxEntriesReturned="10"></tns:IndexedPageItemView>' +
-      '<tns:ParentFolderIds>' + 
-        '<t:DistinguishedFolderId Id="inbox"></t:DistinguishedFolderId>' + 
-      '</tns:ParentFolderIds>' + 
+      '<tns:ParentFolderIds>' +
+        '<t:DistinguishedFolderId Id="inbox"></t:DistinguishedFolderId>' +
+      '</tns:ParentFolderIds>' +
     '</tns:FindItem>';
 
   exports.client.FindItem(soapRequest, function(err, result, body) {
@@ -85,9 +85,9 @@ exports.getEmails = function(folderName, limit, callback) {
       if (responseCode !== 'NoError') {
         return callback(new Error(responseCode));
       }
-        
+
       var rootFolder = result['s:Body']['m:FindItemResponse']['m:ResponseMessages']['m:FindItemResponseMessage']['m:RootFolder'];
-      
+
       var emails = [];
       rootFolder['t:Items']['t:Message'].forEach(function(item, idx) {
         var md5hasher = crypto.createHash('md5');
@@ -140,7 +140,7 @@ exports.getEmail = function(itemId, callback) {
     return callback(new Error('Id is not correct.'));
   }
 
-  var soapRequest = 
+  var soapRequest =
     '<tns:GetItem xmlns="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">' +
       '<tns:ItemShape>' +
         '<t:BaseShape>Default</t:BaseShape>' +
@@ -164,7 +164,7 @@ exports.getEmail = function(itemId, callback) {
       if (responseCode !== 'NoError') {
         return callback(new Error(responseCode));
       }
-       
+
       var item = result['s:Body']['m:GetItemResponse']['m:ResponseMessages']['m:GetItemResponseMessage']['m:Items']['t:Message'];
 
       var itemId = {
@@ -231,29 +231,119 @@ exports.getFolders = function(id, callback) {
     id = 'inbox';
   }
 
-  var soapRequest = 
-    '<tns:FindFolder xmlns:tns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
+  var soapRequest =
+    '<tns:FindFolder Traversal="Shallow" xmlns:tns="http://schemas.microsoft.com/exchange/services/2006/messages">' +
         '<tns:FolderShape>' +
           '<t:BaseShape>Default</t:BaseShape>' +
         '</tns:FolderShape>' +
-        '<tns:ParentFolderIds>' + 
-          '<t:DistinguishedFolderId Id="inbox"></t:DistinguishedFolderId>' + 
-        '</tns:ParentFolderIds>' + 
+        '<tns:ParentFolderIds>' +
+          '<t:DistinguishedFolderId Id="inbox"></t:DistinguishedFolderId>' +
+        '</tns:ParentFolderIds>' +
       '</tns:FindFolder>';
 
   exports.client.FindFolder(soapRequest, function(err, result) {
     if (err) {
       callback(err)
     }
-    
+
     if (result.ResponseMessages.FindFolderResponseMessage.ResponseCode == 'NoError') {
       var rootFolder = result.ResponseMessages.FindFolderResponseMessage.RootFolder;
-      
+
       rootFolder.Folders.Folder.forEach(function(folder) {
         // console.log(folder);
       });
 
       callback(null, {});
     }
+  });
+}
+
+
+exports.getCalendarItems = function(limit, callback) {
+  var soapRequest =
+    '<tns:GetFolder>' +
+      '<tns:FolderShape>' +
+        '<t:BaseShape>IdOnly</t:BaseShape>' +
+      '</tns:FolderShape>' +
+      '<tns:FolderIds>' +
+      '<t:DistinguishedFolderId Id="calendar" />' +
+      '</tns:FolderIds>' +
+    '</tns:GetFolder>'
+
+  exports.client.FindItem(soapRequest, function(err, result, body) {
+    if (err) {
+      console.log("error in exchanger");
+      return callback(err);
+    }
+
+    var parser = new xml2js.Parser();
+    var folderId;
+    var changeKey;
+
+    parser.parseString(body, function(err, result) {
+      var responseCode = result['s:Body']['m:GetFolderResponse']['m:ResponseMessages']['m:GetFolderResponseMessage']['m:ResponseCode'];
+
+      if (responseCode !== 'NoError') {
+        return callback(new Error(responseCode));
+      }
+
+      var folderItem = result['s:Body']['m:GetFolderResponse']['m:ResponseMessages']['m:GetFolderResponseMessage']['m:Folders']['t:CalendarFolder']['t:FolderId'];
+      folderId = folderItem['@'].Id;
+      changeKey = folderItem['@'].ChangeKey;
+    });
+
+    // First request succeeded and received folderId and changeKey.
+    // Now get calendatrItems
+
+    var soapRequest2 =
+      '<tns:FindItem Traversal="Shallow">' +
+        '<tns:ItemShape>' +
+          '<t:BaseShape>IdOnly</t:BaseShape>' +
+          '<t:AdditionalProperties>' +
+            '<t:FieldURI FieldURI="item:Subject" />' +
+            '<t:FieldURI FieldURI="calendar:Start" />' +
+            '<t:FieldURI FieldURI="calendar:End" />' +
+          '</t:AdditionalProperties>' +
+        '</tns:ItemShape>' +
+        '<tns:CalendarView MaxEntriesReturned="5" StartDate="2013-08-21T17:30:24.127Z" EndDate="2014-09-20T17:30:24.127Z" />' +
+        '<tns:ParentFolderIds>' +
+          '<t:FolderId Id="'+folderId+'" ChangeKey="'+changeKey+'" />' +
+        '</tns:ParentFolderIds>' +
+      '</tns:FindItem>'
+
+    exports.client.FindItem(soapRequest2, function(err, result, body) {
+      if (err) {
+        console.log("error in exchanger");
+        return callback(err);
+      }
+
+      parser.parseString(body, function(err, result) {
+        var responseCode = result['s:Body']['m:FindItemResponse']['m:ResponseMessages']['m:FindItemResponseMessage']['m:ResponseCode'];
+        if (responseCode !== 'NoError') {
+        return callback(new Error(responseCode));
+        }
+
+        var rootFolder = result['s:Body']['m:FindItemResponse']['m:ResponseMessages']['m:FindItemResponseMessage']['m:RootFolder'];
+
+        var calendarItems = [];
+        rootFolder['t:Items']['t:CalendarItem'].forEach(function(item, idx) {
+
+          var itemId = {
+            id: item['t:ItemId']['@'].Id,
+            changeKey: item['t:ItemId']['@'].ChangeKey
+          };
+
+          var dateTimeReceived = item['t:DateTimeReceived'];
+
+          calendarItems.push({
+            id: itemId,
+            subject: item['t:Subject'],
+            start: item['t:Start'],
+            end: item['t:End']
+          });
+        });
+        callback(null, calendarItems);
+      });
+    });
   });
 }
